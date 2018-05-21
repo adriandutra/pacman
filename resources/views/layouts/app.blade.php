6a2a25edae1ab18b8c67978da8bed8a62f5a183b
Adrián Alejandro Dutra <adutra@cat-technologies.com>
2018-05-21 09:11:06 -0300

    Se quita la opcion de Registrar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,8 +43,7 @@
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
                         @if (Auth::guest())
-                          <li><a href="{{ route('login') }}" style="color:#FFF">Ingreso</a></li>
-                          <li><a href="{{ route('register') }}" style="color:#FFF">Registro</a></li> 
+                          <li><a href="{{ route('login') }}" style="color:#FFF">Ingreso</a></li>                           
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
